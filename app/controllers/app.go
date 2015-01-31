f8ed1ce1c122e9b01c2db976dcebdf718e97cc71
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-01-31 16:40:51 -0400

    Redirect to success/fail page

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+		"os"
+		"fmt"
 		"github.com/revel/revel"
 )
 
@@ -8,24 +10,41 @@ type App struct {
 	*revel.Controller
 }
 
+type Form struct {
+	Name, Email, Subject, Message string
+}
+
 func (c App) Index() revel.Result {
 	return c.Render()
 }
 
-func send_email(destination_email string, sender_name string, sender_email string, sender_subject string, sender_message string) {
-
-}
-
-// Show the hotel information.
-func (c App) Show() revel.Result {
-	var id string
-	c.Params.Bind(&id, "id")
-	return c.Render(id)
-}
-
-// Show the hotel information.
 func (c App) Contact() revel.Result {
-	var id string
-	c.Params.Bind(&id, "id")
-	return c.Render(id)
+	id := c.Params.Get("id")
+	form := Form{Name: c.Params.Get("name"), 
+				 Email: c.Params.Get("email"),
+				 Subject: c.Params.Get("subject"),
+				 Message: c.Params.Get("message")}
+    successfully_sent := send_email(form)
+    if successfully_sent	{
+    	return c.Redirect(App.Success)
+    }	else	{
+    	return c.Redirect(App.Failure)
+    }
+	return c.Render(id, form, successfully_sent)
+}
+
+func (c App) Success() revel.Result {
+	c.Response.Status = 200
+	return c.Render()
+}
+
+func (c App) Failure() revel.Result {
+	c.Response.Status = 400
+	return c.Render()
+}
+
+func send_email(form Form) bool {
+	os_val := os.Getenv("")
+	fmt.Printf("%s\n", os_val)
+	return true
 }
