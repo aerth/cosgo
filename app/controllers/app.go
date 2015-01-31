ea62a2a90665cb950a6a5a4fac775642a939b3e6
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-01-31 14:55:28 -0400

    Properly configured route for submitting form

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,8 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+		"github.com/revel/revel"
+)
 
 type App struct {
 	*revel.Controller
@@ -10,24 +12,20 @@ func (c App) Index() revel.Result {
 	return c.Render()
 }
 
-func (c App) Contact(name string, email string, message string) revel.Result {
-	
-	var id string = c.Params.Get("id")
+func send_email(destination_email string, sender_name string, sender_email string, sender_subject string, sender_message string) {
 
-	if len(id) > 0 {
-		// Some email was provided
-	}else{
-		var token string = c.Params.Get("token")
-		if len(token) > 0{
-			// A token was provided. Attempt to convert the token into a user email
-			// If the email exists, continue. Else, return an error
-			c.Response.Status = 418
-			return c.Render()
-		}
-	}
-	c.Response.Status = 418
-	return c.Render()
-	//send the email
-	//var email_status_code int = pass_along_form(destination_email, message_email, message_subject, message_body)
-	//return email_status_code
+}
+
+// Show the hotel information.
+func (c App) Show() revel.Result {
+	var id string
+	c.Params.Bind(&id, "id")
+	return c.Render(id)
+}
+
+// Show the hotel information.
+func (c App) Contact() revel.Result {
+	var id string
+	c.Params.Bind(&id, "id")
+	return c.Render(id)
 }
