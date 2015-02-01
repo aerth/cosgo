2773ea6e609cdadafc8cf889fdb2108568cba865
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-01-31 20:51:11 -0400

    Forms are now sent via Mandrill

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,9 +1,10 @@
 package controllers
 
 import (
-		"os"
-		"fmt"
-		"github.com/revel/revel"
+	"fmt"
+	mandrill "github.com/keighl/mandrill"
+	"github.com/revel/revel"
+	"os"
 )
 
 type App struct {
@@ -19,18 +20,26 @@ func (c App) Index() revel.Result {
 }
 
 func (c App) Contact() revel.Result {
-	id := c.Params.Get("id")
-	form := Form{Name: c.Params.Get("name"), 
-				 Email: c.Params.Get("email"),
-				 Subject: c.Params.Get("subject"),
-				 Message: c.Params.Get("message")}
-    successfully_sent := send_email(form)
-    if successfully_sent	{
-    	return c.Redirect(App.Success)
-    }	else	{
-    	return c.Redirect(App.Failure)
-    }
-	return c.Render(id, form, successfully_sent)
+	email := c.Params.Get("id")
+	form := Form{Name: c.Params.Get("name"),
+		Email:   c.Params.Get("email"),
+		Subject: c.Params.Get("subject"),
+		Message: c.Params.Get("message")}
+	//invalid_id = validate_destination_email(email)
+	//if invalid_id {
+	//	return c.Redirect(App.InvalidEmail)
+	//}
+	//invalid_form = validate_form(form)
+	//if valid_form {
+	//
+	//}
+	successfully_sent := send_email(email, form)
+	if successfully_sent {
+		return c.Redirect(App.Success)
+	} else {
+		return c.Redirect(App.Failure)
+	}
+	return c.Render(email, form, successfully_sent)
 }
 
 func (c App) Success() revel.Result {
@@ -43,8 +52,26 @@ func (c App) Failure() revel.Result {
 	return c.Render()
 }
 
-func send_email(form Form) bool {
-	os_val := os.Getenv("")
-	fmt.Printf("%s\n", os_val)
+func send_email(destination string, form Form) bool {
+	MANDRILL_KEY := os.Getenv("MANDRILL_KEY")
+	if len(MANDRILL_KEY) == 0 {
+		fmt.Printf("API Key for Mandrill was not found.\nSet the environment variable MANDRILL_KEY with a valid API key.")
+		return false
+	}
+	client := mandrill.ClientWithKey(MANDRILL_KEY)
+
+	message := &mandrill.Message{}
+	message.AddRecipient(destination, destination, "to")
+	message.FromEmail = form.Email
+	message.FromName = form.Name
+	if len(form.Subject) == 0{
+		form.Subject = "New contact form submission!"
+	}
+	message.Subject = form.Subject
+	message.HTML = "<p>" + form.Message + "<p>"
+	message.Text = form.Message
+
+	responses, apiError, err := client.MessagesSend(message)
+	fmt.Printf("%s %s %s\n", responses, apiError, err)
 	return true
 }
