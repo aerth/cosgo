52d9808efdfc90c571b0c856961929f6f035dd16
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-02-02 20:52:06 -0400

    Configuring Heroku...

--- app/controllers/contact.go
+++ app/controllers/contact.go
@@ -1,9 +1,9 @@
 package controllers
 
 import (
-	mandrill "github.com/keighl/mandrill"
+	//mandrill "github.com/keighl/mandrill"
 	"github.com/revel/revel"
-	"fmt"
+	//"fmt"
 )
 
 type Contact struct {
@@ -34,44 +34,44 @@ func (c Contact) Contact() revel.Result {
 
 func sendEmail(destination string, form Form) bool {
 	
-	mandrillKey, found := revel.Config.String("mandrillKey")
-	if !found {
-		panic("Mandrill API key not set in app.conf.")
-	}
-	if len(mandrillKey) == 0 {
-		panic("Mandrill API key is empty.")
-	}
+	// mandrillKey, found := revel.Config.String("mandrillKey")
+	// if !found {
+	// 	panic("Mandrill API key not set in app.conf.")
+	// }
+	// if len(mandrillKey) == 0 {
+	// 	panic("Mandrill API key is empty.")
+	// }
 	
-	client := mandrill.ClientWithKey(mandrillKey)
+	// client := mandrill.ClientWithKey(mandrillKey)
 
-	message := &mandrill.Message{}
-	message.AddRecipient(destination, destination, "to")
-	message.FromEmail = form.Email
-	message.FromName = form.Name
-	if len(form.Subject) == 0{
-		form.Subject = "New contact form submission!"
-	}
-	message.Subject = form.Subject
-	message.HTML = "<p>" + form.Message + "<p>"
-	message.Text = form.Message
+	// message := &mandrill.Message{}
+	// message.AddRecipient(destination, destination, "to")
+	// message.FromEmail = form.Email
+	// message.FromName = form.Name
+	// if len(form.Subject) == 0{
+	// 	form.Subject = "New contact form submission!"
+	// }
+	// message.Subject = form.Subject
+	// message.HTML = "<p>" + form.Message + "<p>"
+	// message.Text = form.Message
 
-	responses, apiError, err := client.MessagesSend(message)
+	// responses, apiError, err := client.MessagesSend(message)
 	
-	if err != nil {
-		panic(err)
-	}
-	if apiError != nil {
-		fmt.Printf("\n\n\n\nAPI ERROR\n%s\n\n\n\n", apiError)
-		return false
-	}
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// if apiError != nil {
+	// 	fmt.Printf("\n\n\n\nAPI ERROR\n%s\n\n\n\n", apiError)
+	// 	return false
+	// }
 	
-	length := len(responses)
-	for i := 0; i < length; i++ {
-		if responses[i].Status == "rejected" {
-			return false
-		} else if responses[i].Status == "invalid" {
-			return false
-		}
-	}
+	// length := len(responses)
+	// for i := 0; i < length; i++ {
+	// 	if responses[i].Status == "rejected" {
+	// 		return false
+	// 	} else if responses[i].Status == "invalid" {
+	// 		return false
+	// 	}
+	// }
 	return true
 }
