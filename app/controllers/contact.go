4d99af8c2c3023992a07099b749dcf7ac658d261
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-02-04 20:15:56 -0400

    Refactored MongoLab connection process, MongoDB calls

--- app/controllers/contact.go
+++ app/controllers/contact.go
@@ -3,12 +3,64 @@ package controllers
 import (
 	"fmt"
 	"math/rand"
-	//mandrill "github.com/keighl/mandrill"
+	mandrill "github.com/keighl/mandrill"
 	"time"
 	"github.com/revel/revel"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
+
+var (
+    mgoSession     *mgo.Session
+    tokenCollection *mgo.Collection
+    mandrillKey string
+)
+
+func getSession () *mgo.Session {
+    if mgoSession == nil {
+    	mgoSession = dialMongo()
+    }
+    return mgoSession.Clone()
+}
+
+func getUserCollection () *mgo.Collection {
+	if tokenCollection == nil {
+		session := getSession()
+		tokenCollection = session.DB("static-contact").C("static-contact-tokens")
+	}
+	return tokenCollection
+}
+
+func getMandrillKey () string {
+	if mandrillKey == "" {
+		var found bool
+		mandrillKey, found = revel.Config.String("mandrillKey")
+		if !found {
+			panic("Mandrill API key not set in app.conf.")
+		}
+		if len(mandrillKey) == 0 {
+			panic("Mandrill API key is empty.")
+		}
+	}
+	return mandrillKey
+}
+
+func dialMongo () *mgo.Session {
+	mongoUri, mongoFound := revel.Config.String("mongoLabUri")
+	
+	if !mongoFound {
+		panic("Mongo URI not set in app.conf.")
+	}
+	if len(mongoUri) == 0 {
+		panic("Mongo URI is invalid with length 0")
+	}
+
+    mgoSession, err := mgo.Dial(mongoUri)
+    if err != nil {
+         panic(err)
+    }
+    return mgoSession.Clone()
+}
 
 type Contact struct {
 	*revel.Controller
@@ -39,6 +91,16 @@ func (c Contact) Contact() revel.Result {
 	}
 }
 
+func (c Contact) Register() revel.Result {
+	email := c.Params.Get("email")
+	return c.Render(email)
+}
+
+func (c Contact) RegisteredEmail() revel.Result {
+	email := c.Params.Get("ids")
+	return c.Render(email)
+}
+
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 var tokenLength = 8
 
@@ -50,7 +112,7 @@ func generateToken() string {
 	return string(b)
 }
 
-func createTokenRecord(email string) bool {
+func createEmailRecord(email string) bool {
 	// Create a new user
 	var token string
 	for {
@@ -63,54 +125,22 @@ func createTokenRecord(email string) bool {
 	_, month, _ := time.Now().Date()
 
 	newRecipient := recipientRecord {Token: token, 
-									 Email:email, 
-									 MonthlyCount: 0, 
+									 Email: email, 
+									 MonthlyCount: 1, 
 									 CurrentMonth: int(month)}
-	
 	// Add the user to the DB
-	mongoUri, mongoFound := revel.Config.String("mongoLabUri")
-	if !mongoFound {
-		panic("Mongo URI not set in app.conf.")
-	}
-	if len(mongoUri) == 0 {
-		panic("Mongo URI is invalid with length 0")
-	}
-
-	session, err := mgo.Dial(mongoUri)
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-	c := session.DB("static-contact").C("static-contact-tokens")
-	err = c.Insert(&newRecipient)
+	c := getUserCollection()
+	err := c.Insert(&newRecipient)
 	if err != nil {
 	    fmt.Printf("%s\n", err)
 	}
-	fmt.Printf("%s %s %d %d\n", newRecipient.Token, newRecipient.Email, newRecipient.MonthlyCount, newRecipient.CurrentMonth)
 	return true
 }
 
 func tokenExistsInDB(token string) bool {
-	mongoUri, mongoFound := revel.Config.String("mongoLabUri")
-	
-	if !mongoFound {
-		panic("Mongo URI not set in app.conf.")
-	}
-	if len(mongoUri) == 0 {
-		panic("Mongo URI is invalid with length 0")
-	}
-	session, err := mgo.Dial(mongoUri)
-	if err != nil {
-		panic(err)
-	}
-	
-	defer session.Close()
-	c := session.DB("static-contact").C("static-contact-tokens")
-	
+	c := getUserCollection()
 	result := recipientRecord{}
-	
-	err = c.Find(bson.M{"Token": token}).One(&result)
-	
+	err := c.Find(bson.M{"Token": token}).One(&result)
 	if err != nil {
 		// The token does not exist
 		return false
@@ -118,103 +148,57 @@ func tokenExistsInDB(token string) bool {
 	return true
 }
 
-func incrementSentEmailCount(token string) bool {
-	
-	mongoUri, mongoFound := revel.Config.String("mongoLabUri")
-	
-	if !mongoFound {
-		panic("Mongo URI not set in app.conf.")
-	}
-	if len(mongoUri) == 0 {
-		panic("Mongo URI is invalid with length 0")
-	}
-	session, err := mgo.Dial(mongoUri)
-	if err != nil {
-		panic(err)
-	}
-	
-	defer session.Close()
-	c := session.DB("static-contact").C("static-contact-tokens")
-	
+func incrementSentEmailCount(email string) bool {
+	c := getUserCollection()
 	oldRecord := recipientRecord{}
-	
-	err = c.Find(bson.M{"token": token}).One(&oldRecord)
-	
+	err := c.Find(bson.M{"email": email}).One(&oldRecord)
+	if err != nil {
+		createEmailRecord(email)
+		return true
+	}
 	newRecord := oldRecord
 	newRecord.MonthlyCount += 1
-	err = c.Update(bson.M{"token": token}, &newRecord)
-
-	return true
-}
-
-func sendEmail(token string, form Form) bool {
-	
-	mongoUri, mongoFound := revel.Config.String("mongoLabUri")
-	if !mongoFound {
-		panic("Mongo URI not set in app.conf.")
-	}
-	if len(mongoUri) == 0 {
-		panic("Mongo URI is invalid with length 0")
-	}
-	session, err := mgo.Dial(mongoUri)
+	err = c.Update(bson.M{"email": email}, &newRecord)
 	if err != nil {
 		panic(err)
 	}
+	return true
+}
 
-	defer session.Close()
-	c := session.DB("static-contact").C("static-contact-tokens")
+func sendEmail(destinationEmail string, form Form) bool {
+	emailKey := getMandrillKey()
 
-	result := recipientRecord{}
+	client := mandrill.ClientWithKey(emailKey)
+
+	message := &mandrill.Message{}
+	message.AddRecipient(destinationEmail, destinationEmail, "to")
+	message.FromEmail = form.Email
+	message.FromName = form.Name
+	if len(form.Subject) == 0{
+		form.Subject = "New contact form submission!"
+	}
+	message.Subject = form.Subject
+	message.HTML = "<p>" + form.Message + "<p>"
+	message.Text = form.Message
+
+	responses, apiError, err := client.MessagesSend(message)
 	
-	err = c.Find(bson.M{"token": token}).One(&result)
 	if err != nil {
-		// The token does not exist
-		fmt.Printf("The token does not exist!! %s\n", token)
+		panic(err)
+	}
+	if apiError != nil {
+		fmt.Printf("\n\n\n\nAPI ERROR\n%s\n\n\n\n", apiError)
 		return false
 	}
-
-	// destinationEmail := result.Email
-
-	// mandrillKey, found := revel.Config.String("mandrillKey")
-	// if !found {
-	// 	panic("Mandrill API key not set in app.conf.")
-	// }
-	// if len(mandrillKey) == 0 {
-	// 	panic("Mandrill API key is empty.")
-	// }
 	
-	// client := mandrill.ClientWithKey(mandrillKey)
-
-	// message := &mandrill.Message{}
-	// message.AddRecipient(destinationEmail, destinationEmail, "to")
-	// message.FromEmail = form.Email
-	// message.FromName = form.Name
-	// if len(form.Subject) == 0{
-	// 	form.Subject = "New contact form submission!"
-	// }
-	// message.Subject = form.Subject
-	// message.HTML = "<p>" + form.Message + "<p>"
-	// message.Text = form.Message
-
-	// responses, apiError, err := client.MessagesSend(message)
-	
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// if apiError != nil {
-	// 	fmt.Printf("\n\n\n\nAPI ERROR\n%s\n\n\n\n", apiError)
-	// 	return false
-	// }
-	
-	// length := len(responses)
-	// for i := 0; i < length; i++ {
-	// 	if responses[i].Status == "rejected" {
-	// 		return false
-	// 	} else if responses[i].Status == "invalid" {
-	// 		return false
-	// 	}
-	// }
-	// increment record
-	incrementSentEmailCount(token)
+	length := len(responses)
+	for i := 0; i < length; i++ {
+		if responses[i].Status == "rejected" {
+			return false
+		} else if responses[i].Status == "invalid" {
+			return false
+		}
+	}
+	incrementSentEmailCount(destinationEmail)
 	return true
 }
