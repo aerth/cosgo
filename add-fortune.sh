fed7425608ac56ede125cb887dba6168114b426f
aerth <aerth@riseup.net>
2016-10-08 12:26:45 -0700

    Fix fortunes.sh

--- add-fortune.sh
+++ add-fortune.sh
@@ -1,6 +1,39 @@
 #!/bin/sh
 # double line separated fortunes. some get cut off but oh well.
 echo "Populating fortunes.txt, press Ctrl C when you think its big enough."
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
+
 for i in $(cat fortunes.txt); do 
-fortune -o >> fortunes.txt && echo "" >> fortunes.txt;
+fortune >> fortunes.txt && echo "" >> fortunes.txt;
 done
