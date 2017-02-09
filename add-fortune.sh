d4638abdc444f23b1870f3d58ec73d65cbed9ad0
aerth <aerth@riseup.net>
2017-02-09 11:15:54 -0800

    Require 'fortune' program to be installed in $PATH

--- add-fortune.sh
+++ add-fortune.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # double line separated fortunes. some get cut off but oh well.
+if [ -z $(which fortune)]; then
+echo "fortune program is not in \$PATH"
+exit 1
+fi
+
 echo "Populating fortunes.txt, press Ctrl C when you think its big enough."
 fortune >> fortunes.txt && echo "" >> fortunes.txt;
 fortune >> fortunes.txt && echo "" >> fortunes.txt;
